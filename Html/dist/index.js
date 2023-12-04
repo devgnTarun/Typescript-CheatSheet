27dc6cce5e2cb78255680e7de1b92bb3a2fc1550
Tarun Devgan <107904243+devgnTarun@users.noreply.github.com>
2023-12-04 23:24:38 +0530

    Basic datatypes

---
+++ Html/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+// Datatypes Ts
+// let name = 'Tarun'; Name is shown already declared because its inside the window object , which you can check in javascript
+// let naam:string = 'Tarun'; Easy formula
+// let naam = <string> 'Tarun'; Another syntax to get it now , what is that
+// let naam:any = 'Tarun'; 
+// naam = 9; ----------------> In this case Typescript knows its any, so, it can be changed from string to number type
+// let naam:string = "Tarun";
+// const nameLength = naam.length; ---> Using ts leads to more understandable code and better, which reccomends more method on its type
+// Also it have union types --------------------------
+// let naam:string | number = 'Tarun'
+// let naam:string | number = 999;
+// let naam:string | number = false; ---------> But it can't be undefined, null or boolean 
+// console.log(nameLength);
